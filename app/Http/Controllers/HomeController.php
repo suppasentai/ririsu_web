309ab6bfd640ef1fa00baf0c168811b0a8b3f33a
Cao Duc <cao.son.duc@sun-asterisk.com>
2019-12-06 21:18:18 +0700

    xong config

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,8 @@ class HomeController extends Controller
     public function index()
     {
         Redis::set('name', 'Taylor');
-        dd(phpinfo());
+        $values = Redis::get('name');
+        dd($values);
         return view('home');
     }
 
