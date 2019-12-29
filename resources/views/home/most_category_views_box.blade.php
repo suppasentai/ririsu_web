bfbebdcd15206e62692b88d954c2222831a8dc9a
Cao Duc <cao.son.duc@sun-asterisk.com>
2019-12-30 01:56:51 +0700

    ok_4

--- resources/views/home/most_category_views_box.blade.php
+++ resources/views/home/most_category_views_box.blade.php
@@ -3,8 +3,8 @@
         <div class="news-post image-post">
             <img src="{{$politic->image}}" alt="">
             <div class="hover-box">
-                <a href="#" class="category category-world">{{$politic->category_ref}}</a>
-                <h2><a href="single-post.html">{{$politic->title}}</a></h2>
+                <a  class="category category-world">{{$politic->category_ref}}</a>
+                <h2><a href="{{ route('releases.show', ['slug' => $politic->slug])}}">{{$politic->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$politic->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($politic)->count()}}</li>
@@ -19,7 +19,7 @@
             <img src="{{$business->image}}" alt="">
             <div class="hover-box">
                 <a href="#" class="category">{{$business->category_ref}}</a>
-                <h2><a href="single-post.html">{{$business->title}}</a></h2>
+                <h2><a href="{{ route('releases.show', ['slug' => $business->slug])}}">{{$business->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$business->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($business)->count()}}</li>
@@ -34,7 +34,7 @@
             <img src="{{$lifestyle->image}}" alt="">
             <div class="hover-box">
                 <a href="#" class="category category-fashion">{{$lifestyle->category_ref}}</a>
-                <h2><a href="single-post.html">{{$lifestyle->title}}</a></h2>
+                <h2><a href="{{ route('releases.show', ['slug' => $lifestyle->slug])}}">{{$lifestyle->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$lifestyle->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($lifestyle)->count()}}</li>
@@ -49,7 +49,7 @@
             <img src="{{$education->image}}" alt="">
             <div class="hover-box">
                 <a href="#" class="category category-food">{{$education->category_ref}}</a>
-                <h2><a href="single-post.html">{{$education->title}}</a></h2>
+                <h2><a href="{{ route('releases.show', ['slug' => $education->slug])}}">{{$education->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$education->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($education)->count()}}</li>
@@ -64,7 +64,7 @@
             <img src="{{$sport->image}}" alt="">
             <div class="hover-box">
                 <a href="#" class="category category-sport">{{$sport->category_ref}}</a>
-                <h2><a href="single-post.html">{{$sport->title}}</a></h2>
+                <h2><a href="{{ route('releases.show', ['slug' => $sport->slug])}}">{{$sport->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$sport->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($sport)->count()}}</li>
@@ -79,7 +79,7 @@
             <img src="{{$tech->image}}" alt="">
             <div class="hover-box">
                 <a href="#" class="category category-tech">{{$tech->category_ref}}</a>
-                <h2><a href="single-post.html">{{$tech->title}}</a></h2>
+                <h2><a href="{{ route('releases.show', ['slug' => $tech->slug])}}">{{$tech->title}}</a></h2>
                 <ul class="post-tags">
                     <li><i class="lnr lnr-apartment"></i><a href="#">{{$tech->company->title}}</a></li>
                     <li><i class="lnr lnr-eye"></i>{{views($tech)->count()}}</li>
