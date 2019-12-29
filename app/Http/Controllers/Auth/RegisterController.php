0a07c9125a1358e77eb000a33d0b4de06bba479b
Cao Duc <cao.son.duc@sun-asterisk.com>
2019-12-30 02:04:09 +0700

    ok_99

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Role;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -78,6 +79,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $customer = Role::where('slug', 'customer')->first();
         $user->roles()->attach($customer);
         return $user;
     }
